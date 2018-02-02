8b413bfbb5ec66bb4850b406f9faa61791a6621f
liruidong <286182399@qq.com>
2018-02-02 18:23:11 +0800

    creat project

---
+++ src/components/Cart.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cart">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cart',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+</style>
